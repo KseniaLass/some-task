c52e04fdb5e90022598f7c7a83e44e4ad1e2eae5
KDekho01 <ksenia.dekhonova@isobar.ru>
2017-07-31 18:17:59 +0300

    add valiation

--- index.js
+++ index.js
@@ -1,31 +1,76 @@
 class Form {
-    contstructor($form) {
+    constructor($form) {
         this.form = $form;
 
         this.form.find('#submitButton').on('click', (e) => {
             e.preventDefault();
-            return false
-           this.submit();
+            this.submit();
         });
     }
     __validate() {
         //=> { isValid: Boolean, errorFields: String[] }
+        let data = this.__getData();
+
+        let errorFields = [];
+
+        if (!this.__checkFIO(data.fio)) {
+            errorFields.push('fio');
+        }
+        if(!this.__checkPhone(data.phone)) {
+            errorFields.push('phone');
+        }
+        if(!this.__checkEmail(data.email)) {
+            errorFields.push('email');
+        }
+        this.__checkEmail(data.email);
     }
     __getData() {
         //=> Object
+        let inputsValue = {};
+
+        this.form.find('input').each(function() {
+            let name = $(this).attr('name'),
+                type = $(this).attr('type'),
+                value = $(this).val();
+           if(type !== 'submit') {
+               inputsValue[name] = value;
+           }
+        });
+        return inputsValue;
     }
-    setData(Object) {
+    __setData(Object) {
         //=> undefined
+    }
+    __checkFIO(value) {
+        if(value.split(' ').length === 3) {
+            return true
+        }
+    }
+    __checkPhone(value) {
+        let replaceAllSpace = value.replace(/\s/g, ''),
+            replaceAllExceptNumber = value.replace(/[^\d]/g, ''),
+            regexp = /^\+7\(\d{3}\)(\d{3})-(\d{2})-(\d{2})/,
+            amount = 0;
+
+        for(var i=0; i < replaceAllExceptNumber.length; i++) {
+            let number = parseInt(replaceAllExceptNumber[i]);
+            amount = amount + number;
+        }
+
+        if(regexp.test(replaceAllSpace) && amount <= 30) {
+            return true
+        }
+    }
+    __checkEmail(value) {
+        let regexp = /^([a-zA-Z0-9_.+-])+\@(ya(ndex)?\.)+(ru|ua|by|kz|com)/g;
+        if(regexp.test(value)) {
+            return true
+        }
     }
     submit() {
         //=> undefined
-        console.log('here');
+        this.__validate();
     }
 }
 
 var myForm = new Form($('#myForm'));
-
-$('#submitButton').on('click', (e) => {
-    e.preventDefault();
-    myFormExample.submit();
-});
